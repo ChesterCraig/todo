f4dff405710b543f1130c7f4b6fe9a4455420bd2
ChesterCraig <chestercraig@Chesters-MacBook-Pro.local>
2017-04-23 14:02:18 +1200

    added support for heroku based postgresql db

--- db/pg.js
+++ db/pg.js
@@ -1,10 +1,19 @@
 var pg = require('pg');
 var fs = require('fs');
 
-//Parse configuration file containing info needed to connect to database
-var config = JSON.parse(fs.readFileSync('./db/config.txt', 'utf8'));
+//Get connection information for database
+if (process.env.DATABASE_URL) {
+    //Heroku hosted database
+    var connectionString = process.env.DATABASE_URL
+} else {
+    //Connect to database
+    var config = JSON.parse(fs.readFileSync('./db/config.txt', 'utf8'));
+    var connectionString = `postgres://${config.username}:${config.password}@${config.location}`;
+}
 
-var connectionString = `postgres://${config.username}:${config.password}@${config.location}`;
+console.log(`Database connection: ${connectionString}`);
+
+//Parse configuration file containing info needed to connect to database
 var client = new pg.Client(connectionString); 
 client.connect();
 
@@ -28,32 +37,3 @@ client.initTodoSchema = function (callback) {
 }
 
 module.exports = {client};
-
-
-
-// var pg = require('pg');
- 
-// // instantiate a new client 
-// // the client will read connection information from 
-// // the same environment variables used by postgres cli tools 
-// var client = new pg.Client();
- 
-// // // connect to our database 
-// // client.connect(function (err) {
-// //   if (err) throw err;
- 
-// //   // execute a query on our database 
-// //   client.query('SELECT $1::text as name', ['brianc'], function (err, result) {
-// //     if (err) throw err;
- 
-// //     // just print the result to the console 
-// //     console.log(result.rows[0]); // outputs: { name: 'brianc' } 
- 
-// //     // disconnect the client 
-// //     client.end(function (err) {
-// //       if (err) throw err;
-// //     });
-// //   });
-// // });
-
-// module.exports = {client};
